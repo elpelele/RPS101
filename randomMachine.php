63aafecdac4689b24d243cb956e9a4a6062b0295
Armoredbrain <l1ght3rth4nl1ght@gmail.com>
2019-10-14 13:34:57 +0200

    type in randomMAchine

--- randomMachine.php
+++ randomMachine.php
@@ -4,4 +4,4 @@ function randomMachine($n,$l)
     $bytes = openssl_random_pseudo_bytes($l);
     $randomNumber = hexdec(bin2hex($bytes));
     return ($randomNumber <= $n) ? $randomNumber:randomMachine($n,$l);
-}cd
+}
